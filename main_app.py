c93031e1e20784d229f104a5e0ebf598272efc28
Veronika Juránková <104098977+xjurv01@users.noreply.github.com>
2022-07-14 20:47:47 +0200

    Add files via upload

--- main_app.py
+++ main_app.py
@@ -32,7 +32,7 @@ if page == 'Mapa':
     from_hour_morning = col1.slider('Rano od', min_value = 5, max_value =12, value = 5)
     to_hour_morning = col1.slider('Rano do', min_value = 5, max_value =12, value=9)
 
-    col1.write('Pocatecni stanice rano mezi {} a {}').format(from_hour_moning,
+    col1.write('Pocatecni stanice rano mezi {} a {}').format(from_hour_morning,
                                                              to_hour_morning)
         query_morning = """ SELECT
                                 start_station_latitude as lat,
@@ -45,7 +45,8 @@ if page == 'Mapa':
         df_bikes_morning = pd.read_sql(sql=query_morning, con=engine)
     col1.map(df_bikes_morning)
 
-    col2.write('Pocatecni stanice odpoledne mezi 15 a 19')
+    col2.write('Pocatecni stanice odpoledne mezi {} a {}').format(from_hour_afternoon,
+                                                                    to_hour_afternoon)
     from_hour_afternoon = col1.slider2('Vecer od', min_value = 12, max_value =23, value = 15)
     to_hour_afternoon = col1.slider2('Vecer do', min_value = 12, max_value =23, value=19)
 
