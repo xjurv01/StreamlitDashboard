8dbd49d7f7d66c5b1b053709a10b7f983f52d8d9
Veronika Juránková <104098977+xjurv01@users.noreply.github.com>
2022-07-14 20:19:10 +0200

    Add files via upload

--- main_app.py
+++ main_app.py
@@ -18,14 +18,14 @@ query_morning = """ SELECT
                         start_station_longitude as lon
                     FROM edinburgh_bikes
                     WHERE hour(started_at) BETWEEN 6 AND 9
-                    LIMIT 20000
+                    LIMIT 30000
                 """
 query_afternoon = """  SELECT
                             start_station_latitude as lat,
                             start_station_longitude as lon
                         FROM edinburgh_bikes
                         WHERE hour(started_at) BETWEEN 15 AND 19
-                        LIMIT 20000
+                        LIMIT 30000
                  """
 
 df_bikes_morning = pd.read_sql(sql=query_morning, con=engine)
@@ -40,11 +40,13 @@ st.title('Moje prvni appka')
 page = st.sidebar.radio('Select page',['Mapa','Thomson'])
 
 if page == 'Mapa':
+    col1, col2 = st.columns(2)
     st.header('Mapa pouzivani sdilenych kol v Edinburgu')
-    st.write('Pocatecni stanice rano mezi 6 a 9')
-    st.map(df_bikes_morning)
-    st.write('Pocatecni stanice odpoledne mezi 15 a 19')
-    st.map(df_bikes_afternoon)
+
+    col1.write('Pocatecni stanice rano mezi 6 a 9')
+    col1.map(df_bikes_morning)
+    col2.write('Pocatecni stanice odpoledne mezi 15 a 19')
+    col2.map(df_bikes_afternoon)
 
 if page == 'Thomson':
     st.write('Thomson sampling')
